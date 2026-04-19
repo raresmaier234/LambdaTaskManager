c2237d6638cc130e417e90738384e6a1e09e2d78
raresmaier234 <raresmaier123@gmail.com>
2026-04-19 19:47:55 +0300

    Add S3 backend configuration to provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
 
+  backend "s3" {
+    bucket         = "lambda-task-cloud-tf-state-557187998171"
+    key            = "lambda-task-cloud/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "lambda-task-cloud-tf-state-lock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
